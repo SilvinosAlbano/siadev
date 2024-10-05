3ad088713ee4017e23f76734d1ea9cd41f329d2c
Silvino de Jesus Albano <itsilvinoalbano@gmail.com>
2024-10-05 20:54:39 +0900

    Update- Import Data
    Aumenta tan funsaun create user bainhira upload husi excel

--- sia_dev/app/Imports/StudentImport.php
+++ sia_dev/app/Imports/StudentImport.php
@@ -2,9 +2,11 @@
 namespace App\Imports;
 
 use App\Models\ModelStudent;
+use APP\Models\ModelUser;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Carbon\Carbon;
-
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 class StudentImport implements ToModel
 {
     public function model(array $row)
@@ -16,14 +18,26 @@ class StudentImport implements ToModel
             return null;
         }
     
-        return new ModelStudent([
+        $student = ModelStudent::create([
+            'id_student'      => (string) Str::uuid(),
             'complete_name'   => $row[0],
             'gender'          => $gender,
             'place_of_birth'  => $row[2],
             'date_of_birth'   => $this->formatDate($row[3]),
             'nre'             => $row[4],
-            'start_year'             => $row[5],
+            'start_year'      => $row[5],
         ]);
+
+
+        ModelUser::create([
+            'user_id'         => (string) Str::uuid(),
+            'username'        => $student->nre, // Use student's NRE as username
+            'email'           => null, // Set email to null or pass an email if available
+            'password'        => Hash::make('defaultpassword'), // Default password
+            'docente_id_student' => $student->id_student, // Link to student
+            'tipo_usuario'    => 'Estudante', // User type as 'Estudante'
+        ]);
+
     }
     
     // Validate gender value
